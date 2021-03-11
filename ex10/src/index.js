7bc8a3bff4f4e1cc182a53dd5a16809fd771376e
Adipx <adipasic86@gmail.com>
2021-03-11 19:51:03 +0100

    .

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+    // Only change code below this line
+   var myFirstName = "Adi";
+   var myLastName = "Pasic";
+   var myHomeTown = "Sarajevo";
+
+    // Only change code above this line
+
+ return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+ };
+
+}
+console.log(main());
+module.exports = main;
