57ba65e58c1850d73054c42384cdbc6c22f4f405
Adipx <adipasic86@gmail.com>
2021-03-11 17:14:17 +0100

    .

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,7 +1,7 @@
 function main(numOne, numTwo){
 var quotient;
 if(numTwo === 0){
-    quotient = "Divide by 0! Cannot divide by zero!";
+    quotient = 'Divide by 0! Cannot divide by zero!';
     }else{ 
 // Only change code below this line
 quotient = numOne / numTwo
@@ -13,4 +13,4 @@ console.log(main(15, 3));
 console.log(main(25, 5));
 console.log(main(420, 10));
 console.log(main(55, 0)); 
-module.exports = main
+module.exports = main;
